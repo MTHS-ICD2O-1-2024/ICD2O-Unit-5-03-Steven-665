76c758c130a2268458d030ada012f0387b832b51
Yi-Steven-Zhou <yi.zhou@stu.ocsb.ca>
2025-04-25 16:15:51 +0000

    add icon

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: yi.zhou
+// Created on: Apr 2025
 // This file contains the JS functions for index.html
+/**
+ * * This function checks if the user has selected the random number from above
+*/
+// eslint-disable-next-line no-unused-vars
+function findMovieType() {
+  // input
+  const userage = parseInt(document.getElementById('age').value)
+
+  // process
+  if (userage => 18) {
+    // output
+    document.getElementById('movie type').innerHTML =
+      '<p>You can go see an NC-17 rated movie alone</p>'
+  } else if (userage => 17) {
+    document.getElementById('movie type').innerHTML =
+      '<p>You can go see an R rated movie alone</p>'
+  } else if (userage => 13) {
+    document.getElementById('movie type').innerHTML =
+      '<p>You can go see a PG-13 rated movie alone</p>'
+  } else if (userage => 13) {
+    document.getElementById('movie type').innerHTML =
+      '<p>You can go see a G and PG rated movie alone</p>'
+  } else {
+    document.getElementById('movie type').innerHTML =
+      '<p>Uh, you are too young for most things</p>'
+  }
+}
